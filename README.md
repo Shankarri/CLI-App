bc02272492cc782ee624d9d34f455441a3056f04
Shanki <shanki.ranju@gmail.com>
2019-02-28 23:49:36 -0600

    updated

--- README.md
+++ README.md
@@ -13,6 +13,10 @@ ________________________________________
 
  * Clone the files to your system
 
+ * Copy all the sql commands from the "bamazon.sql" and run it in your mysql database to run this application.
+
+ * You have to also change the port settings in your file to match your mysql database.
+
  * Go to your terminal and enter below commands to install all the dependent packages
 
 ```
@@ -32,15 +36,18 @@ ________________________________________
 
 * If you do not want to continue, then press No. This will exit the application.
 
-* Screenshot : ![Customer](images/0_customer.PNG)
+* Screenshot : 
+![Customer](images/0_customer.PNG)
 
 * If you want to continue, then press Yes or Y. This will  place an order in the bamazon and your Total Cost of the purchase will be displayed in the terminal. 
 
-* Screenshot : ![Customer](images/1_customer.PNG)
+* Screenshot : 
+![Customer](images/1_customer.PNG)
 
 * If number of units of the selected product is not in stock then it will display Insufficient quantity message.
 
-* Screenshot: ![Customer](images/2_customer.PNG)
+* Screenshot: 
+![Customer](images/2_customer.PNG)
 
 ________________________________________
 
@@ -49,7 +56,8 @@ ________________________________________
 
 1. If the user entered Product Id is not present in bamazon, then display error message.
 
-* Screenshot : ![Customer](images/1_ErrorMsg_customer.PNG)
+* Screenshot : 
+![Customer](images/1_ErrorMsg_customer.PNG)
 
 2. If the user entered Product Id or Number of items are not correct, then display error message.
 
@@ -59,3 +67,79 @@ ________________________________________
 
 ________________________________________
 
+________________________________________
+
+## Manager View
+
+* Command --  node bamazonCustomer
+
+* Once the command is entered, you will see a List a set of menu options:
+
+    1. View Products for Sale
+    2. View Low Inventory
+    3. Add to Inventory
+    4. Add New Product
+
+#### View Products for Sale & View Low Inventory
+
+ 
+* Move the arrow keys to select from one of the options. 
+
+* If you select the first option, then you see the all the products that are available in bamazon.
+
+* If you select the second option, then you see the all the products that has stock quantity less than 5 available in bamazon.
+
+* Screenshot for first and second option: 
+![Customer](images/1_manager.PNG)
+
+
+#### Add to Inventory
+
+* If you select third option, then terminal will first display all the prodcusts that are available in the bamazon. Then you will be asked for product Id and number of stocks to add.
+
+* After entring the correct details and If you want to continue, then press Yes or Y. Stock will be added to the entered product ID. 
+
+* Screenshot : 
+![Customer](images/2_manager.PNG)
+
+________________________________________
+
+### Error Scenario's:
+
+
+1. If the user entered Product Id is not present in bamazon, then display error message.
+
+* Screenshot : 
+![Customer](images/1_ErrorMsg_manager.PNG)
+
+2. If the user entered Product Id or Number of items are not correct, then display error message.
+
+* Screenshot :
+ ![Customer](images/2_ErrorMsg_manager.PNG)
+ ![Customer](images/3_ErrorMsg_manager.PNG)
+
+________________________________________
+
+#### Add New product
+
+* If you select fourth option, then terminal will first display all the Exsisting prodcusts that are available in the bamazon. 
+
+* Then you will be asked for to details for the new Product like Name, Department, price and Stock.
+
+* After entring the correct details and If you want to continue, then press Yes or Y. Stock will be added to the entered product ID. 
+
+* Screenshot : 
+![Customer](images/3_manager.PNG)
+
+________________________________________
+
+### Error Scenario's:
+
+
+1. If the user entered Product Price or Stock are not correct, then display error message.
+
+* Screenshot :
+ ![Customer](images/4_ErrorMsg_manager.PNG)
+ ![Customer](images/5_ErrorMsg_manager.PNG)
+
+________________________________________
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,7 +1,10 @@
+
+// Initialize all the dependent packages
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 var cTable = require("console.table");
 
+// Setup the mysql connection to connect to the database.
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -10,25 +13,33 @@ var connection = mysql.createConnection({
     database: "bamazon"
 });
 
+// Make connection and if there is error in connection, then display 
 connection.connect(function (err) {
     if (err) throw err;
     // console.log("connected as id " + connection.threadId);
     itemsDisplay();
 });
 
+// As soon as the app is run, show the product details from the database to the customer.
 let itemsDisplay = function () {
+    //Run query to fetch all the details from Products table 
     connection.query("SELECT * FROM products", function (err, res) {
+        // If there is error in running the products, then show error
         if (err) throw err;
         var values = [];
+        // Loop through results of the database query and push the results into Values array
         for (var index in res) {
             values.push([' ', res[index].item_id, res[index].product_name, res[index].price]);
         }
         console.log("\n ")
+        // Use the table package to display the Product details in the table format.
         console.table([' ', 'Product Id', 'Product Name', 'Price'], values);
+        // After displaying, ask user for order details
         getUserInput(res);
     });
 }
 
+// Getting Product Id and number of items for placing the order for the user.
 let getUserInput = function (prodDetails) {
     inquirer.prompt([
         {
@@ -48,13 +59,17 @@ let getUserInput = function (prodDetails) {
             default: true
         }])
         .then(function (inquirerResponse) {
+            // If the user confirm to proceed further, then proceed with error checks
             if (inquirerResponse.confirm) {
                 console.log("\n Loading ........\n");
                 var foundprod = false;
                 var userProd;
-                var numItems = parseInt(inquirerResponse.numItems);
-                var productId = parseInt(inquirerResponse.prodId);
-                if (numItems == inquirerResponse.numItems && productId == inquirerResponse.prodId) {
+                var numItems = parseInt(inquirerResponse.numItems.trim());
+                var productId = parseInt(inquirerResponse.prodId.trim());
+
+                // Check if the user has entered number for product Id and number of items
+                if (numItems == inquirerResponse.numItems.trim() && productId == inquirerResponse.prodId.trim()) {
+                    // Check the product details from the database fetching, for the user entered product Id 
                     for (var index in prodDetails) {
                         if (prodDetails[index].item_id == productId) {
                             foundprod = true;
@@ -62,30 +77,36 @@ let getUserInput = function (prodDetails) {
                             break;
                         }
                     }
+                    // If the user has correctly entered the product ID from the product list
                     if (foundprod) {
-                        // console.log("\n ------ Found your product -------\n");
-                        // console.log("\n \n",  userProd);
+                        // If the user entered quantity is not available in bamazon then display error message
                         if (numItems >= userProd.stock_quantity) {
                             console.log("\n **  Insufficient quantity!! We don't have " + numItems + " units of the product in stock for this product. ** \n");
                             connection.end();
                         }
+                        // If the user entered quantity is available in bamazon 
                         else {
                             console.log("\n Updating your Purchase Order");
                             var stockLeftAfterOrder = userProd.stock_quantity - numItems;
+                            // update the stock quantity in database
                             placeOrder(userProd.item_id, stockLeftAfterOrder);
+                            // Display the total cost of thier purchase
                             console.log("\n *** Total Cost of the purchase : \t", numItems * userProd.price);
                         }
                     }
+                    // Check if the user entered product Id is not in the list
                     else {
                         console.log("\n *** Entered Product id is not present  *** \n");
                         connection.end();
                     }
                 }
+                // Check if the user has entered is not integer then display error message
                 else {
                     console.log("\n *** Please enter correct numbers for Product Id and Stock Quantity \n");
                     connection.end();
                 }
             }
+            // If user do not want to proceed in placing order, then display message
             else {
                 console.log("\n Come back Soon !! Run the app again if you wish to buy products\n");
                 connection.end();
@@ -94,8 +115,9 @@ let getUserInput = function (prodDetails) {
 
 }
 
+// Get the product Id and stock from the user input and update the products table
 function placeOrder(prodId, stock_left) {
-    var query = connection.query(
+    connection.query(
         "UPDATE products SET ? WHERE ?;",
         [
             {
